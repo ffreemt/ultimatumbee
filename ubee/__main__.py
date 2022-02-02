844aef29a453cda9b6e641d3365105046099894e
freemt <yucongo+fmt@gmail.com>
2022-02-02 22:07:37 +0800

    Update ubee uclas

--- ubee/__main__.py
+++ ubee/__main__.py
@@ -1,9 +1,14 @@
 """Gen ubee main."""
 # pylint: disable=unused-import, wrong-import-position
 
+from typing import Tuple
+
+from pathlib import Path
 import sys
-from itertools import zip_longest
-from textwrap import dedent
+from random import shuffle
+
+# from itertools import zip_longest
+# from textwrap import dedent
 
 import gradio as gr
 
@@ -18,6 +23,7 @@ if "." not in sys.path:
 
 from ubee.ubee import ubee
 
+logzero.loglevel(10)
 ic_install()
 ic.configureOutput(
     includeContext=True,
@@ -32,7 +38,11 @@ def greet1(name):
     return "Hello " + name + "!!"
 
 
-def greet(text1, text2) -> pd.DataFrame:
+def greet(
+    text1,
+    text2,
+    thresh: float
+) -> Tuple[pd.DataFrame, pd.DataFrame]:
     """Take inputs, return outputs.
 
     Args:
@@ -44,12 +54,25 @@ def greet(text1, text2) -> pd.DataFrame:
     res1 = [elm.strip() for elm in text1.splitlines() if elm.strip()]
     res2 = [elm.strip() for elm in text2.splitlines() if elm.strip()]
 
-    _ = pd.DataFrame(zip_longest(res1, res2), columns=["text1", "text2"])
-    return _
+    # _ = pd.DataFrame(zip_longest(res1, res2), columns=["text1", "text2"])
+    # return _
+
+    res1_, res2_ = ubee(res1, res2, thresh)
+
+    return pd.DataFrame(res1_, columns=["text1", "text2", "likelihood"]), pd.DataFrame(res2_, columns=["text1", "text2"])
 
 
 def main():
     """Create main entry."""
+    text_zh = Path("data/test_zh.txt").read_text("utf8")
+    text_en = [
+        elm.strip()
+        for elm in Path("data/test_en.txt").read_text("utf8").splitlines()
+        if elm.strip()
+    ]
+    shuffle(text_en)
+    text_en = "\n\n".join(text_en)
+
     title = "Ultimatumbee Aligner"
     theme = "dark-grass"
     description = """WIP showcasing a novel aligner"""
@@ -62,20 +85,8 @@ def main():
 
     lines = 15
     placeholder = "Type or paste text here"
-    default1 = dedent(
-        """
-        test 1
-        abc
-        love you
-        """
-    )
-    default2 = dedent(
-        """
-        爱你
-        甲乙丙
-        测试 1
-        """
-    )
+    default1 = text_zh
+    default2 = text_en
     label1 = "text1"
     label2 = "text2"
     inputs = [
@@ -84,6 +95,12 @@ def main():
         ),
         gr.inputs.Textbox(
             lines=lines, placeholder=placeholder, default=default2, label=label2
+        ),
+        gr.inputs.Slider(
+            minimum=0.0,
+            maximum=1.0,
+            step=0.1,
+            default=0.5,
         ),
     ]
 
@@ -95,8 +112,26 @@ def main():
         type="auto",
         label="To be aligned",
     )
-    outputs = [  # tot. 1
+    aligned = gr.outputs.Dataframe(
+        headers=None,
+        max_rows=lines,  # 20
+        max_cols=None,
+        overflow_row_behaviour="paginate",
+        type="auto",
+        label="Aligned",
+    )
+    leftover = gr.outputs.Dataframe(
+        headers=None,
+        max_rows=lines,  # 20
+        max_cols=None,
+        overflow_row_behaviour="paginate",
+        type="auto",
+        label="Leftover",
+    )
+    outputs = [  # tot. 3
         out_df,
+        aligned,
+        leftover,
     ]
 
     iface = gr.Interface(
--- ubee/ubee.py
+++ ubee/ubee.py
@@ -1,7 +1,41 @@
-"""Function for gr's fn."""
-# pylint=disable=
+"""Align via ubee,"""
+# pylint: disable=
+from typing import Iterable, List, Tuple
+from itertools import zip_longest
+
+from logzero import logger
+from ubee.uclas import uclas
 
 
-def ubee():
-    """Gen a dummy."""
-    ...
+def ubee(
+    sents_zh: Iterable,
+    sents_en: Iterable,
+    thresh: float = 0.5,
+) -> Tuple[List[Tuple[str, str, float]], List[Tuple[str, str]]]:
+    """Align blocks.
+
+    Args:
+        sents_zh: list of text, can be any langauge supported by clas-l-user
+        sents_zh: ditto
+    Returns:
+        three tuples of aligned blocked
+        leftovers (unaligned)
+    """
+    res = []
+    labels = [*sents_en]
+
+    lo1 = []
+    lo2 = labels[:]
+
+    for seq in sents_zh:
+        label, likelihood = uclas(seq, labels, thresh=thresh)
+        if label:
+            res.append((seq, label, likelihood))
+            try:
+                lo2.remove(label)
+            except Exception as exc:
+                logger.error(exc)
+                logger.info("seq: %s, lable: %s", seq, label)
+        else:
+            lo1.append(seq)
+    return res, [*zip_longest(lo1, lo2)]
