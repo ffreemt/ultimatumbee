1c932ec979410c91bbb7e40d82e36e6bb18d6e3a
ffreemt <yucongo+fmt@gmail.com>
2022-07-01 19:49:07 +0800

    Update nodemon app.py setup

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import sys
 from ubee import __main__
 
 if __name__ == "__main__":
-        sys.exit(__main__())
+        sys.exit(__main__.main())
 
---
+++ run-nodemon-python-app.sh
@@ -0,0 +1,1 @@
+nodemon -e py -w app.py -w ubee -x python app.py
